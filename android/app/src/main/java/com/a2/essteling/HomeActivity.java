ce629c84790c2873fa8f8351db7709b588bf5f01
Coen <cc.denboer1@student.avans.nl>
2022-05-31 14:35:39 +0200

    added low fidelity home screen width buttons to scanner and shop

--- android/app/src/main/java/com/a2/essteling/HomeActivity.java
+++ android/app/src/main/java/com/a2/essteling/HomeActivity.java
@@ -2,7 +2,9 @@ package com.a2.essteling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String LOG_TAG = HomeActivity.class.getSimpleName();
@@ -11,5 +13,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onShopButton(View view){
+        Intent intent = new Intent(this, ShopActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onScannerButton(View view){
+        Intent intent = new Intent(this,PassActivity.class);
+        startActivity(intent);
     }
 }
