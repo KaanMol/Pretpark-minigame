6e14d8dddbb9998b4746c1fa7078603adf2c648d
Lucas <somfic@gmail.com>
2022-06-02 16:08:15 +0200

    Register cards through accounts

--- android/app/src/main/java/com/a2/essteling/PassActivity.java
+++ android/app/src/main/java/com/a2/essteling/PassActivity.java
@@ -3,6 +3,15 @@ package com.a2.essteling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class PassActivity extends AppCompatActivity {
     private static final String LOG_TAG = PassActivity.class.getSimpleName();
@@ -11,5 +20,33 @@ public class PassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass);
+
+        Button button = findViewById(R.id.buttonConnect);
+        TextView textName = findViewById(R.id.textName);
+        TextView textCard = findViewById(R.id.textCard);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        button.setOnClickListener(click -> {
+            String accountId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+            String name = textName.getText().toString();
+            String cardId = textCard.getText().toString();
+
+            Log.i(LOG_TAG, "AccountId: " + accountId);
+            Log.i(LOG_TAG, "Name: " + name);
+
+            String url = "https://mobiele-beleving-dev.herokuapp.com/cards?accountId=" + accountId + "&cardId=" + cardId;
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    response -> {
+                        Log.i(LOG_TAG, "Response: " + response);
+                        Database.setName(cardId, name);
+                    },
+                    error -> {
+                        Log.i(LOG_TAG, "Error: " + error);
+                    });
+
+            requestQueue.add(stringRequest);
+        });
     }
 }
