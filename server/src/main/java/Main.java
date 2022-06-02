5160dd1d203e469a7416c282ba157eba0259ae40
Lucas <somfic@gmail.com>
2022-06-02 10:06:28 +0200

    Select port from environment variable

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -6,7 +6,14 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            Server server = new Server(8000);
+            int port = 8000;
+
+            String rawPort = System.getenv("PORT");
+            if (rawPort != null) {
+                port = Integer.parseInt(rawPort);
+            }
+
+            Server server = new Server(port);
             server.route("/cards", new CardHandler());
             server.route("/points", new PointsHandler());
             server.route("/products", new ProductHandler());
