084334ee512c13f6a603f9a041f2a7ece859bc38
koen <kvoostrom011@gmail.com>
2022-06-02 09:24:03 +0200

    added scorebord layout
    added scoreboard layout begin

---
+++ android/app/src/main/java/com/a2/essteling/activity_scoreboard.java
@@ -0,0 +1,14 @@
+package com.a2.essteling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_scoreboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scoreboard);
+    }
+}
