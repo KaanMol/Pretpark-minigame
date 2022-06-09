dc83483aaba06a5c206d13b9bcd5179851e0cf24
Momin2004 <m.musa@student.avans.nl>
2022-06-09 12:36:34 +0200

    added description coupon

--- android/app/src/main/java/com/a2/essteling/Shop/CouponActivity.java
+++ android/app/src/main/java/com/a2/essteling/Shop/CouponActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.a2.essteling.R;
 import com.bumptech.glide.Glide;
@@ -11,6 +12,8 @@ import com.bumptech.glide.Glide;
 public class CouponActivity extends AppCompatActivity {
     private ImageView itemImage;
     private ShopItem item;
+    private TextView itemDescription;
+    private TextView itemName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,10 @@ public class CouponActivity extends AppCompatActivity {
         setContentView(R.layout.activity_coupon);
         this.item = (ShopItem) getIntent().getExtras().get("item");
         itemImage = findViewById(R.id.itemImageBuy);
+        itemDescription = findViewById(R.id.itemDescription);
+        itemDescription.setText(item.getDescription());
+        itemName = findViewById(R.id.itemNameBuy);
+        itemName.setText(item.getName());
         if (item.getImageLocal() == -1) {
             Glide.with(this).load(item.getImage()).into(itemImage);
         } else {
